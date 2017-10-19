c6d8f84d6c6bdf09a879666d2ba63eaca871cf15
jinco13 <jinco13@users.noreply.github.com>
2017-10-19 22:43:35 +0900

    lec23

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 
-import SearchForm from './SearchForm'
+import SearchForm from './SearchForm';
+import GeocodeResult from './GeocodeResult';
 class App extends Component {
   constructor(props){
     super(props);
@@ -17,6 +18,8 @@ class App extends Component {
       <div>
         <h1>緯度経度検索</h1>
         <SearchForm onSubmit={place => this.handlePlaceSubmit(place)}/>
+        <GeocodeResult
+          address={this.state.address}/>
       </div>
     );
   }
