cc774b813556a9b69f08cb8601bc17e0803d14e4
jinco13 <jinco13@users.noreply.github.com>
2017-10-20 21:20:46 +0900

    lec 24

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,6 +2,10 @@ import React, {Component} from 'react';
 
 import SearchForm from './SearchForm';
 import GeocodeResult from './GeocodeResult';
+import axios from 'axios';
+
+const GEOCODE_ENDPOINT = "https://maps.googleapis.com/maps/api/geocode/json";
+
 class App extends Component {
   constructor(props){
     super(props);
@@ -9,8 +13,45 @@ class App extends Component {
     };
   }
 
+  setErrorMessage(message){
+    this.setState({
+      address: message,
+      lat: 0,
+      lng: 0,
+    })
+  }
+
   handlePlaceSubmit(place){
-    console.log(place)
+    console.log(place);
+    axios
+      .get(GEOCODE_ENDPOINT, { params: { address: place } })
+      .then((results) => {
+        console.log(results);
+        const data = results.data;
+        const result = results.data.results[0];
+        switch(data.status) {
+          case 'OK': {
+            const location = result.geometry.location;
+            this.setState({
+              address: result.formatted_address,
+              lat: location.lat,
+              lng: location.lng,
+            });
+            break;
+          }
+          case 'ZERO_RESULTS': {
+            this.setErrorMessage("結果が見つかりませんでした。")
+            break;
+          }
+          default: {
+            this.setErrorMessage('エラーが発生しました。');
+            break;
+          }
+        }
+      })
+      .catch(() => {
+        this.setErrorMessage('通信に失敗しました。');
+      });
   }
 
   render(){
@@ -19,7 +60,10 @@ class App extends Component {
         <h1>緯度経度検索</h1>
         <SearchForm onSubmit={place => this.handlePlaceSubmit(place)}/>
         <GeocodeResult
-          address={this.state.address}/>
+          address={this.state.address}
+          lat={this.state.lat}
+          lng={this.state.lng}
+        />
       </div>
     );
   }
