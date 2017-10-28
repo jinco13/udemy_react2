d087fa21291df07177a6ba233657ca1356c9a630
jinco13 <jinco13@users.noreply.github.com>
2017-10-28 21:34:14 +0900

    lec29 wip

--- src/domain/HotelRepository.js
+++ src/domain/HotelRepository.js
@@ -1,3 +1,24 @@
-export const searchHotelByLocation = location =>
-  
-;
+import Rakuten from '../lib/Rakuten';
+
+const RAKUTEN_APP_ID = "1031045483115496208";
+
+export const searchHotelByLocation = (location) => {
+  const params = {
+    applicationId: RAKUTEN_APP_ID,
+    datumType: 1,
+    latitude: location.lat,
+    longtitude: location.lng,
+  };
+  return Rakuten.Travel.simpleHotelSearch(params)
+    .then((result) => {
+      //console.log(result);
+      return result.data.hotels.map((hotel) => {
+        const basicInfo = hotel.hotel[0].hotelBasicInfo;
+        return {
+          id: basicInfo.hotelNo,
+          name: basicInfo.hotelName,
+          url: basicInfo.hotelInformationUrl,
+        };
+      });
+    });
+};
